3b037a477e5d99ff64c4afa25103d65a3ea7fefb
benoit-lavigne <benoit.lavigne@cegepmontpetit.ca>
2022-03-24 13:59:20 -0400

    Fin cours

--- Assets/Scripts/GestionnaireAudio.cs
+++ Assets/Scripts/GestionnaireAudio.cs
@@ -1,26 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GestionnaireAudio : MonoBehaviour
 {
-    private AudioSource musique;
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    [SerializeField]
+    private AudioClip sonFxTest;
+
+    [SerializeField]
+    private Slider glissiereVolumeMusique;
+    
+    [SerializeField]
+    private Slider glissiereVolumeEffets;
+
+    private AudioSource audioSource;
+
 
     private void Start()
     {
-        musique = GetComponent<AudioSource>();
-        musique.volume = 0;
+
+        audioSource = GetComponent<AudioSource>();
+
+        float volume;
+
+        //Passage de parametre par référence
+        audioMixer.GetFloat("VolumeMusique", out volume);
+        glissiereVolumeMusique.value = volume;
+
+
+        audioMixer.GetFloat("VolumeEffets", out volume);
+        glissiereVolumeEffets.value = volume;
+
     }
 
+
+    // Méthode appelée par le slider du volume musique
     public void AjusteVolumeMusique(float volume)
     {
         Debug.Log("Volume musique: " + volume);
 
-        musique.volume = volume;
+        audioMixer.SetFloat("VolumeMusique", volume);
+
     }
     public void AjusteVolumeEffets(float volume)
     {
         Debug.Log("Volume effets: " + volume);
+
+        audioMixer.SetFloat("VolumeEffets", volume);
+
+    }
+
+
+    public void TestVolumeEffet()
+    {
+        audioSource.PlayOneShot(sonFxTest);
     }
 
 
